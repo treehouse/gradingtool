f43328e887b05a57521a1dbe8d46e632c90a256c
dustinusey <dustinusey@me.com>
2022-06-18 09:40:32 -0500

    push

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,38 +1,73 @@
+// data
 const api = {
-    techdegrees: '',
-    
+    techdegrees: 'https://grading-tool-api.herokuapp.com/api/techdegrees',
 }
 
-// left panel
-const leftPanel = {
-    init: {
-        data: [],
-        getData: () => {
-            
-        }
+/**
+ * inital data loading
+ * 
+ * 1. LEFT PANEL
+ * 2. MIDDLE (REQUIREMENTS)
+ * 3. RIGHT PANEL
+ * 
+ */
 
-    },
-    update: () => {
+// 1
+const leftPanelDropdown = document.getElementById('left-panel-dropdown')
+const leftPanelDropdown_list = document.querySelector('#left-panel-dropdown ul');
 
+// leftPanelDropdown.classList.add('show');
+
+fetch(api.techdegrees)
+  .then(response => response.json())
+  .then(data => buildDropdownContent(data))
+
+function buildDropdownContent(data) {
+    data.forEach(techdegree => {
+        let li = document.createElement('li');
+        li.setAttribute("data-td-id", techdegree.id);
+        li.setAttribute("data-td-list-item", 'data-td-list-item');
+        let span = document.createElement('span');
+        span.textContent = techdegree.name;
+        let icon = document.createElement('i');
+        icon.classList = 'fa-solid fa-star';
+        li.appendChild(span);
+        li.appendChild(icon)
+        leftPanelDropdown_list.appendChild(li);
+    });
+    checkLocalStorageForFavoriteTechdegree(data, 0);
+}
+
+function checkLocalStorageForFavoriteTechdegree(data, index) {
+    // if there is no favorite selected, default to first techdegree from API
+    if (!localStorage.favoriteTechdegree) {
+        localStorage.setItem('favoriteTechdegree', JSON.stringify(data[index]));
     }
 }
 
-// middle
-const middle = {
-    init: {
+// function setFavoriteTechdegreeInLocalStorage(data, index) {
+//     if (!localStorage.favoriteTechdegree) {
+//         localStorage.setItem('favoriteTechdegree', JSON.stringify(data[index]))
+//     }
+//     let favoriteTechdegree = localStorage.getItem('favoriteTechdegree');
+//     let favoriteTechdegreeParsed = JSON.parse(favoriteTechdegree);
+//     setFavoriteTechdegreeUI(favoriteTechdegreeParsed.id - 1);
+// }
 
-    },
-    update: () => {
+// function setFavoriteTechdegreeUI(index) {
+//     const dropdownItems = document.querySelectorAll('[data-td-list-item]')
+//     dropdownItems[index].classList.add('favorite');
+// }
 
-    }
-}
+// //  changing favorite techdegree
+// const dropdown = document.getElementById('left-panel-dropdown');
 
-// right panel
-const rightPanel = {
-    init: {
-
-    },
-    update: () => {
-        
-    }
-}
+// dropdown.addEventListener('click', () => {
+//     const dropdownItems = document.querySelectorAll('[data-td-list-item]');
+//     dropdownItems.forEach(item => {
+//         item.addEventListener('click', () => {
+//             let id = item.getAttribute('data-td-id') - 1;
+//             setFavoriteTechdegreeUI(id);
+//         })
+//     })
+// })
